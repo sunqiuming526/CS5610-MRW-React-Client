a57dce49417d2e2ad512a9d156b14b9ce54f8c8f
Qiuming Sun <sunqiuming526@gmail.com>
2020-11-15 13:19:06 +0800

    Added Register Form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Link, Route} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 
 
 import NavbarComponent from "./components/NavbarComponent";
 import MovieListComponent from "./components/MovieListComponent";
+import RegistrationForm from "./components/Login/RegistrationForm/RegistrationForm";
+import AlertComponent from "./components/Login/AlertComponent/AlertComponent";
 
 function App() {
+  const [title, updateTitle] = useState(null)
+  const [errorMessage, updateErrorMessage] = useState(null)
+
   return (
-    <div className="App">
-      <NavbarComponent/>
-      <MovieListComponent/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavbarComponent/>
+        <MovieListComponent/>
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+          </Switch>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
