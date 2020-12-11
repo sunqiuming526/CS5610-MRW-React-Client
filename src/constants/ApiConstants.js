bcd46fea2c15434292fd15b198bd00231c368a50
smartbunny12 <xuefanqi12@gmail.com>
2020-12-10 16:46:05 -0800

    update server link for deploy

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+// export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+export const API_BASE_URL = "https://mrw-final-project-node.herokuapp.com"
+
 export const API_USERS_URL = `${API_BASE_URL}/users`
 export const API_MOVIES_URL = `${API_BASE_URL}/movies`
 export const API_COMMENTS_URL = `${API_BASE_URL}/comments`
